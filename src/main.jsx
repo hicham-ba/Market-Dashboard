faa4ea2c11a55877d37903be9e80e5cdea63bf0f
hicham-ba <hbadawi71@yahoo.com>
2026-03-09 15:56:53 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MarketUniverse from './DowUniverse.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  React.createElement(MarketUniverse)
+);
